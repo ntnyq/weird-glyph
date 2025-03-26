b1bdc0d97bbec87544c5d26e88f11431bd5cba54
ntnyq <ntnyq13@gmail.com>
2025-03-26 08:55:51 +0800

    feat!: drop CJS build, now it's ESM only

--- playground/src/Play.vue
+++ playground/src/Play.vue
@@ -46,27 +46,27 @@ watchEffect(() => {
 
 <template>
   <div class="relative h-full of-y-auto p-4">
-    <div class="relative mx-auto max-w-920px md:p-10">
+    <div class="relative mx-auto my-8 max-w-3xl">
       <input
         v-model.trim="input"
         type="text"
-        class="block w-full border rounded px-4 py-2 text-lg font-medium"
+        class="block w-full border border-base rounded px-4 py-2 text-lg font-medium"
         placeholder="Input something..."
         maxlength="200"
       />
     </div>
 
-    <div class="relative mx-auto max-w-920px break-all md:p-10">
-      <ul class="relative">
+    <div class="relative mx-auto max-w-3xl break-all">
+      <ul class="relative space-y-6">
         <li
           v-for="item in weirdGlyphList"
           :key="item.key"
-          class="relative mb-4 border p-4"
+          class="relative border border-base rounded-md p-6 shadow space-y-3"
         >
-          <h2 class="mb-4 text-xl font-semibold op-75">
+          <p class="text-2xl">{{ item.output || '--' }}</p>
+          <h2 class="text-right text-xl font-semibold op-75">
             {{ item.category }}:{{ item.variant }}
           </h2>
-          <p class="text-2xl">{{ item.output }}</p>
         </li>
       </ul>
     </div>
