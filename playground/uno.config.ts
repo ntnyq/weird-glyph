81478c204de1b7d1e7e643134c407b6bc3bb63e4
ntnyq <ntnyq13@gmail.com>
2025-04-17 15:25:43 +0800

    chore: update

--- playground/uno.config.ts
+++ playground/uno.config.ts
@@ -32,7 +32,7 @@ export default defineConfig({
     'border-bg-base': 'border-$c-bg-base',
     'text-base': 'text-$c-text-base',
     'btn-action':
-      'min-w-[140px] rounded-md bg-$c-text-base px-3 py-2 text-$c-bg-base transition hover:opacity-90',
+      'min-w-140px rounded-md bg-$c-text-base px-3 py-2 text-$c-bg-base transition hover:opacity-90',
     'btn-icon':
       'h-10 w-10 inline-flex items-center justify-center text-lg rounded-md hover:bg-zinc-200 dark:hover:bg-zinc-700',
   },
--- src/data.ts
+++ src/data.ts
@@ -1,4 +1,4 @@
-import weirdGlyphJson from './weird-glyph.json'
+import weirdGlyphJson from './weird-glyph.json' with { type: 'json' }
 import type { WeirdGlyphData } from './types'
 
 export const weirdGlyphData: WeirdGlyphData = weirdGlyphJson
